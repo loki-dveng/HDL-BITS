cd61510eaa5a4ec94f01bed29170ad224c9dd02c
LOKESH A S <as.lokesh1462004@gmail.com>
2026-04-16 13:35:16 +0530

    Create DFF and gates.v

---
+++ SEQUENTIAL/LATHCHES AND FLIP FLOP /DFF and gates.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input x,
+    output z
+); 
+    reg w1,w2,w3;
+    always@(posedge clk) begin
+        w1<=w1^x;
+        w2<=~w2&x;
+        w3<=~w3|x;
+    end
+    assign z=~(w1|w2|w3);
+endmodule
