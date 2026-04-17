b2070527f05978893fc3f46150c7b86293fb72fd
LOKESH A S <as.lokesh1462004@gmail.com>
2026-04-17 18:51:14 +0530

    Create 7420 chip.v

---
+++ Combinational/Basic gates/7420 chip.v
@@ -0,0 +1,11 @@
+module top_module ( 
+    input p1a, p1b, p1c, p1d,
+    output p1y,
+    input p2a, p2b, p2c, p2d,
+    output p2y );
+ wire w1,w2;
+    assign w1=p1a&p1b&p1c&p1d;
+    assign w2=p2a&p2b&p2c&p2d;
+    assign p1y=~w1;
+    assign p2y=~w2;
+endmodule
