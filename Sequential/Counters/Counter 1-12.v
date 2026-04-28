7d6cdcecd83095461b58b9d0302aa07b8f7395d3
LOKESH A S <as.lokesh1462004@gmail.com>
2026-04-28 11:02:08 +0530

    Create Counter 1-12.v

---
+++ Sequential/Counters/Counter 1-12.v
@@ -0,0 +1,14 @@
+module top_module (
+    input clk,
+    input reset,
+    input enable,
+    output [3:0] Q,
+    output c_enable,
+    output c_load,
+    output [3:0] c_d
+); 
+    assign c_enable=enable;
+    assign c_load=reset|(enable&(Q==4'd12));
+    assign c_d=4'd1;
+    count4 u_count4(.clk(clk),.enable(c_enable),.load(c_load),.d(c_d),.Q(Q));
+endmodule
