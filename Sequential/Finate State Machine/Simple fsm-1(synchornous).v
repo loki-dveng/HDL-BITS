f1382364c05a33d5c2fd940033f3ee3237a06485
LOKESH A S <as.lokesh1462004@gmail.com>
2026-04-29 18:20:08 +0530

    Create Simple fsm-1(synchornous).v

---
+++ Sequential/Finate State Machine/Simple fsm-1(synchornous).v
@@ -0,0 +1,27 @@
+// Note the Verilog-1995 module declaration syntax here:
+module top_module(clk, reset, in, out);
+    input clk;
+    input reset;    
+    input in;
+    output out;
+    reg out;
+
+    parameter A=0, B=1; 
+    reg present_state, next_state;
+
+    always@(*) begin   
+        case(present_state)
+            A:next_state=in?A:B;
+            B:next_state=in?B:A;
+            default:next_state=B;
+        endcase
+    end
+    always@(posedge clk ) begin  
+        if(reset)
+            present_state<=B;
+        else
+            present_state<=next_state;
+    end
+    assign out=(present_state==B);
+
+endmodule
