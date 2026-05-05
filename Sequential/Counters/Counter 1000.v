8275a12aa28bf128223476611c502ad9398c5f19
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-05 10:13:09 +0530

    Create Counter 1000.v

---
+++ Sequential/Counters/Counter 1000.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input reset,
+    output OneHertz,
+    output [2:0] c_enable
+); 
+    wire[3:0]q0,q1,q2;
+    assign c_enable[0]=1'b1;
+    assign c_enable[1]=(q0==4'd9);
+    assign c_enable[2]=(q0==4'd9)&(q1==4'd9);
+    bcdcount c0(clk,reset,c_enable[0],q0);
+    bcdcount c1(clk,reset,c_enable[1],q1);
+    bcdcount c2(clk,reset,c_enable[2],q2);
+    assign OneHertz=(q0==4'd9)&(q1==4'd9)&(q2==4'd9);
+endmodule
