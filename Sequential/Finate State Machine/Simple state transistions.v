9e25615a62cd3acab47e866c1a83c35c20021253
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-19 10:05:54 +0530

    Create Simple state transistions.v

---
+++ Sequential/Finate State Machine/Simple state transistions.v
@@ -0,0 +1,18 @@
+module top_module(
+input in,
+input[1:0]state,
+output reg[1:0]next_state,
+output out
+);
+    parameter A=2'd0,B=2'd1,C=2'd2,D=2'd3;
+    always@(*)begin
+    case(state)
+        A:next_state=in?B:A;
+        B:next_state=in?B:C;
+        C:next_state=in?D:A;
+        D:next_state=in?B:C;
+        default:next_state=A;
+    endcase
+end
+assign out=(state==D);
+endmodule
