36ff6be68dc9a9314dd882543220280c1832da18
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-19 10:07:09 +0530

    Create Simple one-hot state transistions.v

---
+++ Sequential/Finate State Machine/Simple one-hot state transistions.v
@@ -0,0 +1,12 @@
+module top_module(
+    input in,
+    input [3:0] state,
+    output [3:0] next_state,
+    output out); 
+    parameter A=0, B=1, C=2, D=3;
+    assign next_state[0]=(state[0]&~in)|(state[2]&~in);
+    assign next_state[1]=(state[0]&in)|(state[1]&in)|(state[3]&in);
+    assign next_state[2]=(state[1]&~in)|(state[3]&~in);
+    assign next_state[3]=(state[2]&in);
+    assign out=state[3];
+endmodule
