d99f8b2dcf61c968c35c538ae05e555a8c4a1305
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-19 09:37:13 +0530

    Create Shift register-1.sv

---
+++ Sequential/Shift Registers/Shift register-1.sv
@@ -0,0 +1,18 @@
+module top_module (
+    input clk,
+    input resetn,   // synchronous reset
+    input in,
+    output out);
+  reg [3:0] q;
+    always@(posedge clk ) begin
+        if(!resetn)
+            q<=4'b0000;
+        else begin
+            q[0]<=in;
+            q[1]<=q[0];
+            q[2]<=q[1];
+            q[3]<=q[2];
+        end
+    end
+    assign out=q[3];
+endmodule
