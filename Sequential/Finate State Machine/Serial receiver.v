4112e37e7b11fca0c6ae8ad6af6b28ed150fe8d4
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-20 12:45:08 +0530

    Create Serial receiver.v

---
+++ Sequential/Finate State Machine/Serial receiver.v
@@ -0,0 +1,44 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,    // Synchronous reset
+    output done
+); 
+localparam IDLE = 4'd0,
+               D0 = 4'd1,
+               D1 = 4'd2,
+               D2 = 4'd3,
+               D3 = 4'd4,
+               D4 = 4'd5,
+               D5 = 4'd6,
+               D6 = 4'd7,
+               D7 = 4'd8,
+               STOP = 4'd9,
+               DONE = 4'd10,
+               WAIT = 4'd11;
+    reg [3:0] state, next_state;
+    always @(*) begin
+        case(state)
+            IDLE:next_state = in?IDLE:D0;
+            D0:next_state = D1;
+            D1:next_state = D2;
+            D2:next_state = D3;
+            D3:next_state = D4;
+            D4:next_state = D5;
+            D5:next_state = D6;
+            D6:next_state = D7;
+            D7:next_state = STOP;
+            STOP:next_state = in?DONE:WAIT;
+            DONE:next_state = in?IDLE:D0; 
+            WAIT:next_state = in?IDLE:WAIT;
+            default:next_state = IDLE;
+        endcase
+    end
+    always@(posedge clk) begin
+        if(reset)
+            state<=IDLE;
+        else
+            state<=next_state;
+    end
+    assign done = (state==DONE);
+endmodule
