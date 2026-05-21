16ad7814f2b11756d0cae61d870e65d12e59ad60
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-21 09:32:43 +0530

    Implement Mealy FSM in Verilog

---
+++ Sequential/Finate State Machine/Design a Mealy FSM.v
@@ -0,0 +1,25 @@
+module top_module (
+    input clk,
+    input aresetn,    // Asynchronous active-low reset
+    input x,
+    output z ); 
+    parameter S0 = 2'd0, 
+              S1 = 2'd1, 
+              S2 = 2'd2;
+    reg [1:0] state,next_state;
+    always@(*) begin
+        case(state)
+            S0:next_state = x?S1:S0;
+            S1:next_state = x?S1:S2;
+            S2:next_state = x?S1:S0;
+            default:next_state = S0;
+        endcase
+    end
+    always@(posedge clk or negedge aresetn) begin
+        if(!aresetn)
+            state <= S0;
+        else
+            state<=next_state;
+    end
+    assign z = (state==S2)&x;
+endmodule
