12cd8661761d95b0ffc04c0b6faf5a81d8728df4
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-25 12:04:59 +0530

    Create PS-2 packet parser.v

---
+++ Sequential/Finate State Machine/PS-2 packet parser.v
@@ -0,0 +1,27 @@
+module top_module(
+    input clk,
+    input [7:0] in,
+    input reset,    // Synchronous reset
+    output done); //
+    parameter BYTE1 = 2'b00,
+              BYTE2= 2'b01,
+              BYTE3=2'b10,
+              DONE=2'b11;
+    reg[1:0] state,next_state;
+    always@(*) begin
+        case (state)
+            BYTE1:next_state=(in[3])?BYTE2:BYTE1;
+            BYTE2:next_state=BYTE3;
+            BYTE3:next_state=DONE;
+            DONE:next_state=(in[3])?BYTE2:BYTE1;
+            default:next_state=BYTE1;
+        endcase
+    end
+    always@(posedge clk) begin
+        if(reset)
+            state<=BYTE1;
+        else
+            state<=next_state;
+    end
+    assign done=(state==DONE);
+endmodule
