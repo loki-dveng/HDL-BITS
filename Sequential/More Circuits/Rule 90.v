f16fc1bd9bd8a53eb3463e09e11d1137e6f78a3f
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-25 16:26:39 +0530

    Create Rule 90.v

---
+++ Sequential/More Circuits/Rule 90.v
@@ -0,0 +1,14 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q ); 
+    always@(posedge clk) begin
+        if(load) begin
+            q<=data;
+        end 
+    else begin
+            q<=(q>>1)^(q<<1);
+        end
+    end
+endmodule
