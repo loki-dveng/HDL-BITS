b994d954383b198ae7f4a276a4fc8e54917a52a1
LOKESH A S <as.lokesh1462004@gmail.com>
2026-05-27 13:30:30 +0530

    Create 1.Timer.v

---
+++ CS450/1.Timer.v
@@ -0,0 +1,16 @@
+module top_module(
+	input clk, 
+	input load, 
+	input [9:0] data, 
+	output tc
+);
+reg[9:0]count;
+    always@(posedge clk)begin 
+        if(load)
+            count<=data;
+        else if(count!=0)
+            count<=count-1;
+        else count<=0;
+    end 
+    assign tc=(count==0);
+endmodule
